7bed462b236a8446a8e384fa7fa5439e2e620d4e
wuchao <wuchao@shiqiao.com>
2021-10-27 16:25:11 +0800

    feat: 🎸 新增config目录

---
+++ config/path.ts
@@ -0,0 +1,6 @@
+/** 图片保存目录 */
+export const IMAGES_STATIC_DIR = '/opt/homebrew/var/www/images';
+/** 静态文件访问域名 */
+export const ACCESS_HOST = 'http://localhost:8080';
+/** 日志保存目录 */
+export const LOG_DIR = '/opt/homebrew/var/log/nestjs'
